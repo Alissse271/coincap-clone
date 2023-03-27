f6658961f2edf7edba510c9438c13592d9f10d4c
Alissse271 <alice_2700@mail.ru>
2023-03-27 23:01:14 +0300

    feat(components): add react hook form

--- src/components/AddToPortfolioModal/AddToPortfolioModal.tsx
+++ src/components/AddToPortfolioModal/AddToPortfolioModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'components';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './styles.scss';
 
 interface Props {
@@ -6,25 +7,46 @@ interface Props {
     toggleModal: () => void;
 }
 
+interface FormValues {
+    value: number;
+}
+
 export const AddToPortfolioModal = ({ isOpenModal, toggleModal }: Props) => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<FormValues>();
+
     const handleClose = () => {
         toggleModal();
-        // reset();
+        reset();
+    };
+
+    const onSubmit: SubmitHandler<FormValues> = (value) => {
+        reset();
     };
 
     return (
         <div className={`modal-background ${isOpenModal ? '' : 'none'}`}>
-            <form className="modal">
+            <form onSubmit={handleSubmit(onSubmit)} className="modal">
                 <div className="modal-header">
                     <h2 className="modal-title">Add to portfolio</h2>
                     <Button type="button" label="x" onClick={handleClose} />
                 </div>
                 <div className="modal-main">
                     <input
+                        {...register('value', {
+                            required: '*value is required',
+                        })}
                         className="modal-main__input"
                         type="number"
                         placeholder="Enter amount"
                     />
+                    {errors.value && (
+                        <p className="input-error">{errors.value.message}</p>
+                    )}
                 </div>
                 <div className="modal-footer">
                     <p className="modal-footer__info">Total: $27971.53</p>
